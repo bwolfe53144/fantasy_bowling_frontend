c65f4c7455b8c59449c894517ff8e4f5858a16a2
bgwolfe <bwolfe53144@yahoo.com>
2025-09-13 16:59:15 -0500

    Stats.jsx showing 0 instead of nan for last year fantasy ppg

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -291,7 +291,8 @@ const Stats = () => {
                         {showLastYear && <td>{lyGames}</td>}
                         {showLastYear && <td>{lyAvg.toFixed(2)}</td>}
                         {showLastYear && <td>{lyPoints}</td>}
-                        {showLastYear && <td>{lyFppg}</td>}                        {!showLastYear && (
+                        {showLastYear && <td>{item.lyFppg ?? "0.00"}</td>}
+                        {!showLastYear && (
                           <td>
                             {item.team && item.teamId ? (
                               <Link to={`/team/${encodeURIComponent(item.teamId)}`}>{item.team}</Link>
