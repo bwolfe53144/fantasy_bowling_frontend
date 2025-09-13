62613fb25f91f675b07e2a1979f67f451ec0ed56
bgwolfe <bwolfe53144@yahoo.com>
2025-09-13 16:57:12 -0500

    Changed it to where it shows 0 instead of Nan for last year fan ppg on the stats.jsx

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -291,8 +291,7 @@ const Stats = () => {
                         {showLastYear && <td>{lyGames}</td>}
                         {showLastYear && <td>{lyAvg.toFixed(2)}</td>}
                         {showLastYear && <td>{lyPoints}</td>}
-                        {showLastYear && <td>{item.lyFppg?.toFixed(2) ?? "0.00"}</td>}
-                        {!showLastYear && (
+                        {showLastYear && <td>{lyFppg}</td>}                        {!showLastYear && (
                           <td>
                             {item.team && item.teamId ? (
                               <Link to={`/team/${encodeURIComponent(item.teamId)}`}>{item.team}</Link>
